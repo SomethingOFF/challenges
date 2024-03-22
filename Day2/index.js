0a05ca650f4dac0ac281c780664b6ca2bf72fece
pratham <somethinoff@gmail.com>
2024-03-22 10:11:06 +0530

    updated

--- Day2/index.js
+++ Day2/index.js
@@ -1,13 +1,25 @@
+const preview = document.getElementById("preview");
 function updatePreview() {
   const topLeft = document.getElementById("topLeft").value;
   const topRight = document.getElementById("topRight").value;
   const bottomRight = document.getElementById("bottomRight").value;
   const bottomLeft = document.getElementById("bottomLeft").value;
 
-  const preview = document.getElementById("preview");
   preview.style.borderTopLeftRadius = `${topLeft}%`;
   preview.style.borderTopRightRadius = `${topRight}%`;
   preview.style.borderBottomRightRadius = `${bottomRight}%`;
   preview.style.borderBottomLeftRadius = `${bottomLeft}%`;
-  preview.textContent =` border-radius: ${topLeft}%  ${topRight}%  ${bottomRight}%  ${bottomLeft}%;`
+  preview.textContent = ` border-radius: ${topLeft}%  ${topRight}%  ${bottomRight}%  ${bottomLeft}%;`;
 }
+
+preview.addEventListener("click", function () {
+  const textToCopy = this.textContent;
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      alert(`Text copied to clipboard: ${textToCopy}`);
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
+});
